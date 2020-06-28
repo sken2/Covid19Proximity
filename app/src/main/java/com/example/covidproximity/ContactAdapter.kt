fe7bfc44c91203d5c75f401aa6435c200b469ec7
SHIMIZU, Kenji <sken2@yahoo.co.jp>
2020-06-28 11:11:30 +0900

    get history list under background

--- app/src/main/java/com/example/covidproximity/ContactAdapter.kt
+++ app/src/main/java/com/example/covidproximity/ContactAdapter.kt
@@ -1,21 +1,47 @@
 package com.example.covidproximity
 
+import android.app.Service
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.database.sqlite.SQLiteDatabase
+import android.os.IBinder
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.covidproximity.model.ContactHistory
+import com.example.covidproximity.tasks.ContactDBService
+import java.util.*
 
-class ContactAdapter(val db : SQLiteDatabase) : RecyclerView.Adapter<ContactAdapter.HistoryHolder>() {
+class ContactAdapter(val db : SQLiteDatabase) :
+    RecyclerView.Adapter<ContactAdapter.HistoryHolder>(),
+    Observer
+{
 
-    val list by lazy {
-        ContactHistory.getAll(db)
+    private var dbService : ContactDBService? = null
+    private val list = mutableListOf<ContactHistory.Contact>()
+    private val afterBind = LinkedList<() -> Unit>()
+
+    init {
+        Log.v(Const.TAG, "ContactAdapter::init")
     }
+
     class HistoryHolder(itemView: ViewGroup) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryHolder {
+        Log.v(Const.TAG, "ContactAdapter::onCreateViewHolder")
         val frame = LayoutInflater.from(parent.context).inflate(R.layout.layout_history_record, parent, false) as ViewGroup
+        with(parent.context.applicationContext) {
+            Intent(parent.context.applicationContext, ContactDBService::class.java).also {
+                bindService(it, connection, Service.BIND_AUTO_CREATE)
+                afterBind.add {
+                    dbService?.resultDispenser?.addObserver(this@ContactAdapter)
+                    dbService?.getAllHistory()
+                }
+            }
+        }
         return HistoryHolder(frame)
     }
 
@@ -30,6 +56,33 @@ class ContactAdapter(val db : SQLiteDatabase) : RecyclerView.Adapter<ContactAdap
         }
         frame.findViewById<TextView>(R.id.view_history_key).also {
             it.text = list.get(position).uuid.toString()
+        }
+    }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        dbService?.resultDispenser?.deleteObserver(this)
+        recyclerView.context.applicationContext.unbindService(connection)
+        super.onDetachedFromRecyclerView(recyclerView)
+    }
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Log.v(Const.TAG, "ContactAdapter::onServiceDisconnected")
+            dbService = null
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Log.v(Const.TAG, "ContactAdapter::onServiceConnected")
+            val binder = service as ContactDBService.LocalBinder
+            dbService = binder.getService()
+            afterBind.forEach{it.invoke()}
+        }
+    }
+
+    override fun update(o: Observable?, arg: Any?) {
+        if (o is ContactDBService.ResultDispenser) {
+            val result = arg as List<ContactHistory.Contact>
+            list.addAll(result)
         }
     }
 }
--- app/src/main/java/com/example/covidproximity/tasks/ContactDBService.kt
+++ app/src/main/java/com/example/covidproximity/tasks/ContactDBService.kt
@@ -2,13 +2,61 @@ package com.example.covidproximity.tasks
 
 import android.app.Service
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import com.example.covidproximity.Const
+import com.example.covidproximity.model.ContactHistory
+import java.util.*
+import java.util.concurrent.Executors
 
-class ContactDBService : Service() {
+class ContactDBService : Service(), Observer {
+
+    private val executor = Executors.newFixedThreadPool(2)
+    lateinit var db : ContactHistory.HistoryDB
+    lateinit var contact : SQLiteDatabase
+    val resultDispenser = ResultDispenser()
+
+    override fun update(o: Observable?, arg: Any?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        db = ContactHistory.HistoryDB(this)
+        contact = db?.readableDatabase
+    }
+
+    override fun onDestroy() {
+        resultDispenser.deleteObservers()
+        contact.close()
+        db.close()
+        super.onDestroy()
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
+        Log.v(Const.TAG, "ContactDBService::onBind")
         return binder
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.v(Const.TAG, "ContactDBService::onUnbind")
+        return super.onUnbind(intent)
+    }
+
+    fun getAllHistory() {
+        val future = executor.submit{
+            val result = ContactHistory.getAll(contact)
+            resultDispenser.complete(result)
+        }
+    }
+
+    class ResultDispenser() : Observable() {
+        fun complete(result : Any) {
+            setChanged()
+            notifyObservers(result)
+        }
     }
 
     inner class LocalBinder() : Binder() {
