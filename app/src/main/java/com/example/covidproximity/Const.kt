c9fd771ee58e718d0bf3a550b872c2f2de27401c
SHIMIZU, Kenji <sken2@yahoo.co.jp>
2020-07-12 18:45:05 +0900

    make const

--- app/src/main/java/com/example/covidproximity/Const.kt
+++ app/src/main/java/com/example/covidproximity/Const.kt
@@ -5,21 +5,26 @@ import java.text.SimpleDateFormat
 
 object Const {
 
-    val TAG = "CoronaProximity"
-    val PREFERENCE_NAME = "CP"
+    const val TAG = "CoronaProximity"
+    const val PREFERENCE_NAME = "CP"
 
     object Preferences {
-        val AUTO_ADVERTISE = "auto_Advertise"
-        val AUTO_SCAN = "auto_scan"
+        const val AUTO_ADVERTISE = "auto_Advertise"
+        const val AUTO_SCAN = "auto_scan"
     }
     val UUID = "0000fd6f-0000-1000-8000-00805f9b34fb"
 
     object Requests {
-        val REQUEST_ENABLE_BT = 123
-        val REQUEST_PREVILEAGES = 456
-        val REQUEST_ENABLE_LOCATION = 789
-        val NOTIFY_FOREGROUND = 999
-        val REQUEST_CODE = 4649
+        const val REQUEST_ENABLE_BT = 123
+        const val REQUEST_PREVILEAGES = 456
+        const val REQUEST_ENABLE_LOCATION = 789
+        const val NOTIFY_FOREGROUND = 999
+        const val REQUEST_CODE = 4649
+    }
+
+    object Keys {
+        const val DetailMode = "detailMode"
+        const val Duration = "duration"
     }
 
     @SuppressLint("SimpleDateFormat")
