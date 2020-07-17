4394f6f4dfbda6f005a08d3e4dbac54c1a44ba37
SHIMIZU, Kenji <sken2@yahoo.co.jp>
2020-07-17 20:24:17 +0900

    clean up

--- app/src/main/java/com/example/covidproximity/Const.kt
+++ app/src/main/java/com/example/covidproximity/Const.kt
@@ -6,11 +6,8 @@ import java.text.SimpleDateFormat
 object Const {
 
     const val TAG = "CoronaProximity"
-    const val PREFERENCE_NAME = "CP"
 
     object Preferences {
-        const val AUTO_ADVERTISE = "auto_Advertise"
-        const val AUTO_SCAN = "auto_scan"
     }
     val UUID = "0000fd6f-0000-1000-8000-00805f9b34fb"
 
